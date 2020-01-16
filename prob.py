41969050a2c76ee97b408663688f4dc47813dc1f
vinayskywalker <vinaykumar.dakshana17@gmail.com>
2020-01-16 11:33:33 +0530

    created some new features

--- prob.py
+++ prob.py
@@ -48,18 +48,47 @@ def main():
     
     for dataset in data:
         dataset['Embarked'].fillna('S',inplace=True)
-        dataset['Fare'].fillna(dataset['Fare'].mean(),inplace=True)
+        dataset['Fare'].fillna(dataset['Fare'].median(),inplace=True)
     
     for dataset in data:
-        dataset.loc[(dataset.Embarked == 'S'),'Embarked']=ord('S')
-        dataset.loc[(dataset.Embarked == 'C'),'Embarked']=ord('C')
-        dataset.loc[(dataset.Embarked == 'Q'),'Embarked']=ord('Q')
+        dataset.loc[(dataset.Embarked == 'S'),'Embarked']=0
+        dataset.loc[(dataset.Embarked == 'C'),'Embarked']=1
+        dataset.loc[(dataset.Embarked == 'Q'),'Embarked']=2
+    
+    # print(train_data_set[['Parch','Survived']].groupby(['Parch'],as_index=False).mean())
 
+
+    for dataset in data:
+        dataset['FamilySize']=dataset['Parch']+dataset['SibSp']+1
+    
+    for dataset in data:
+        dataset['Alone']=0
+        dataset.loc[(dataset['FamilySize']==1,'Alone')]=1
+    
+    
+    divide_agegroup=pd.cut(train_data_set['Age'],5)
+    for dataset in data:
+        dataset.loc[dataset['Age']<=16,'Age']=0
+        dataset.loc[(dataset['Age']>16) & (dataset['Age']<=32),'Age']=1
+        dataset.loc[(dataset['Age']>32) & (dataset['Age']<=48),'Age']=2
+        dataset.loc[(dataset['Age']>48) & (dataset['Age']<=64),'Age']=3
+        dataset.loc[(dataset['Age']>64),'Age']=4
+
+    # print(train_data_set[['Alone','Survived']].groupby(['Alone'],as_index=False).mean())
     
     for dataset in data:
         del dataset['Cabin']
         del dataset['Ticket']
         del dataset['Name']
+        del dataset['Parch']
+        del dataset['SibSp']
+        del dataset['FamilySize']
+
+    
+    print(train_data_set.info())
+    
+
+    
     
 
     Y_train=train_data_set['Survived']
@@ -71,9 +100,12 @@ def main():
     Y_prediction=random_forest.predict(test_data_set)
     abc=test_data_set['PassengerId']
     print(Y_prediction.shape)
-    dict={'x':abc,'y':Y_prediction}
+    dict={'PassengerId':abc,'Survived':Y_prediction}
     df=pd.DataFrame(dict)
     df.to_csv('file.csv')
+
+
+
     
 
     
